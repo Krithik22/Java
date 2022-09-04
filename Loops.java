91f69a0b6f12afc1f1457dcca71ccbe7ed194df4
Krithik22 <krithiksaiprasad22@gmail.com>
2022-09-05 00:39:14 +0530

    while loop

--- Loops.java
+++ Loops.java
@@ -4,8 +4,16 @@ public class Loops {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         // for Loop
+        System.out.println("For Loop:");
         for(int i = 1; i <= 5; i++){
             System.out.println(i);
+        }
+
+        System.out.println("While Loop:");
+        int a = 1;
+        while (a <= 10){
+            System.out.println(a);
+            a++;
         }
         in.close();
     }
