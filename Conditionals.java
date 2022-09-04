8d2b902d4e38d9a79f4e164b05b00fe5d0923108
Krithik22 <krithiksaiprasad22@gmail.com>
2022-09-05 00:35:00 +0530

    conditionals

---
+++ Conditionals.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Salary: ");
+        int sal = input.nextInt();
+        if(sal > 10000){
+            sal += 1000;
+        } else if(sal > 15000){
+            sal += 2000;
+        } else{
+            sal += 500;
+        }
+        System.out.println("Your salary after increment: " + sal);
+    }
+}
