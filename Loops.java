a98fcea9125b5e9beef3c21ccb63e1d14b340943
Krithik22 <krithiksaiprasad22@gmail.com>
2022-09-05 00:37:42 +0530

    for loop code

---
+++ Loops.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // for Loop
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+        in.close();
+    }
+}
