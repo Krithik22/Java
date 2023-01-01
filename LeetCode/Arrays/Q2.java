4efc60fc72b66503cc91cf16d0c05bf12c24f958
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-01 16:03:00 +0530

    Concatenation of array

--- LeetCode/Arrays/Q2.java
+++ LeetCode/Arrays/Q2.java
@@ -1,21 +1,21 @@
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
-// Build Array from permutation
-public class Q1{
+// Concatenation of Array
+public class Q2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(new InputStreamReader(System.in));
         int len = in.nextInt();
         int[] nums = new int[len];
-        int[] ans = new int[nums.length];
+        int[] ans = new int[(nums.length * 2)];
         for (int i = 0; i < nums.length; i++) {
             nums[i] = in.nextInt();
         }
         for (int i = 0; i < nums.length; i++) {
-            int val = nums[nums[i]];
-            ans[i] = val;
+            ans[i] = nums[i];
+            ans[i+len] = nums[i];
         }
-        // System.out.println(Arrays.toString(ans));
-        // In leetcode the input will be given by leetcode
+        System.out.println(Arrays.toString(ans));
     }
 }
