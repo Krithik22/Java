81d75674c75f162e4ac84466674276db2209463f
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-03 20:47:11 +0530

    finished linear search and binary search

---
+++ Algorithms/Searching/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch{
+    static int linearSearch(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            if(target == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,3,9,1};
+        int ans = linearSearch(nums, 5);
+        System.out.println("Element found at " + ans);
+    }
+}
+// Time Complexity: Best case - O(1); Worst Case - O(n)
