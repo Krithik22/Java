0603a42e04d811ab4a12fd0e074837de0c90e76c
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-03 23:42:14 +0530

    completed the create target array problem

--- Algorithms/Searching/OrderAgnosticBS.java
+++ Algorithms/Searching/OrderAgnosticBS.java
@@ -1,46 +1,35 @@
 public class OrderAgnosticBS {
-    static int ascendingOrderBS(int[] arr, int target, int start, int end){
-        while (start <= end) {
-            int mid = start + (end - start)/2;
-            if(target < arr[mid]){
-                end = mid-1;
-            } else if(target > arr[mid]){
-                start = mid+1;
-            } else{
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    static int descendingOrderBS(int[] arr, int target, int start, int end){
-        while (start <= end) {
-            int mid = start + (end - start)/2;
-            if(target < arr[mid]){
-                start = mid+1;
-            } else if(target > arr[mid]){
-                end = mid-1;
-            } else{
-                return mid;
-            }
-        }
-        return -1;
-    }
-
     static int orderAgnosticBS(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
+
+        // checks for the order of the array
         boolean isAsc = arr[start] < arr[end];
-        int ans;
-        if (isAsc) {
-            ans = ascendingOrderBS(arr, target, start, end);
-        } else{
-            ans = descendingOrderBS(arr, target, start, end);
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if (isAsc) {
+                if(target < arr[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
+                }
+            } else{
+                if(target > arr[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
+                }
+            }
         }
-        return ans;
+        
+        return -1;
     }
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int[] nums = {9,8,7,6,5,4,3,2,1};
         int target = 7;
         int ans = orderAgnosticBS(nums, target);
         System.out.println("Element found at index: " + ans);
---
+++ LeetCode/Arrays/CreateTargetArray.java
@@ -0,0 +1,30 @@
+// import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CreateTargetArray{
+    static int[] soln(int[] nums, int[] index){
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int ind = index[i];//Storing the value at index array
+            if (result[ind] != 0) {
+                int temp = result[ind];//defining a temp variable to store the current value at that index 
+                result[ind++] = nums[i];//changing the value at that index and incrementing it
+                while(ind < result.length){
+                    int temp2 = result[ind];
+                    result[ind] = temp;
+                    temp = temp2;
+                    ind++;
+                }
+            } else{
+                result[ind] = nums[i];
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        int[] ans = soln(nums, index);
+        System.out.println(Arrays.toString(ans));
+    }
+}
