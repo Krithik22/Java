f5c11a6e38e94ebda75c3a5f0fdf4d4aa0a3e5ed
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-05 23:07:33 +0530

    added comments to the smallest letter greater prob

--- Algorithms/Searching/BinarySearchProblems/SmallestLetterGreaterThanTarget.java
+++ Algorithms/Searching/BinarySearchProblems/SmallestLetterGreaterThanTarget.java
@@ -18,17 +18,24 @@ public class SmallestLetterGreaterThanTarget {
         return letters[start];
     }
 
+    // Note the letters wrap around that is if there is no element greater than it, then the first element is displayed
     static char soln(char[] letters, char target){
         int start = 0;
         int end = letters.length - 1;
         while (start <= end) {
+            // calculate the mid
             int mid = start + (end - start)/2;
+
+            // If the target character < letters[mid], then change end = mid - 1
             if (target < letters[mid]) {
                 end = mid - 1;
             } else {
+                // else, the target will be greater than it. So start -> mid + 1
                 start = mid + 1;
             }
         }
+        // so we'll have a start value at last, for example if there is no greater element then the start 
+        // is at last index, start % letters.length gives 0
         return letters[start % letters.length];
     }
     public static void main(String[] args) {
