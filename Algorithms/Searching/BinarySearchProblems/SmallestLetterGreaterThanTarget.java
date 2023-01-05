1de8bbedfb3812e9c917cdd865be4843ceb540f8
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-05 18:38:01 +0530

    modified the smallest letter greater than target problem

--- Algorithms/Searching/BinarySearchProblems/SmallestLetterGreaterThanTarget.java
+++ Algorithms/Searching/BinarySearchProblems/SmallestLetterGreaterThanTarget.java
@@ -5,6 +5,9 @@ public class SmallestLetterGreaterThanTarget {
         while (start <= end) {
             int mid = start + (end - start)/2;
             if (letters[mid] == target) {
+                if(mid == letters.length - 1){
+                    mid = -1;
+                }
                 return letters[mid + 1];
             } else if(target > letters[mid]) {
                 start = mid + 1;
@@ -14,10 +17,26 @@ public class SmallestLetterGreaterThanTarget {
         }
         return letters[start];
     }
+
+    static char soln(char[] letters, char target){
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
     public static void main(String[] args) {
         char[] letters = {'c','f','j'};
-        char target = 'c';
-        char ans = nextGreatestLetter(letters, target);
+        char target = 'j';
+        char ans = soln(letters, target);
+        // char ans = nextGreatestLetter(letters, target);
+
         System.out.println(ans);
     }
 }
