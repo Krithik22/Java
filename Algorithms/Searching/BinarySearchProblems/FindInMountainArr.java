41b4f49d7bc6ca09562d598c49661ce311dc9aef
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-07 00:17:17 +0530

    added orderAgnosticBS to the find in mountain arr in place of the binarySearch function

--- Algorithms/Searching/BinarySearchProblems/FindInMountainArr.java
+++ Algorithms/Searching/BinarySearchProblems/FindInMountainArr.java
@@ -15,29 +15,41 @@ public class FindInMountainArr {
         return start;
     }
 
-    static int binarySearch(int[] nums, int target, int start, int end){
+    static int orderAgnosticBS(int[] arr, int target, int start, int end){
         
-        // in this problem we dont know the length of the array so we have to input the start and end
-        // We are finding the range in solution function
-        while (start <= end) {
+
+        // checks for the order of the array
+        boolean isAsc = arr[start] < arr[end];
+        
+        while(start <= end){
             int mid = start + (end - start)/2;
-            if (target < nums[mid]) {
-                end = mid - 1;
-            } else if(target > nums[mid]) {
-                start = mid + 1;
-            } else{
+            if(arr[mid] == target){
                 return mid;
             }
+            if (isAsc) {
+                if(target < arr[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
+                }
+            } else{
+                if(target > arr[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
+                }
+            }
         }
+        
         return -1;
     }
 
     static int FindEleInMountainArr(int[] arr, int target){
         int peak = peakMountainArray(arr);
-        int start = 0;
-        int ans = binarySearch(arr, target, start, peak);
+        
+        int ans = orderAgnosticBS(arr, target, 0, peak);
         if (ans == -1) {
-            ans = binarySearch(arr, target, peak+1, arr.length - 1);
+            ans = orderAgnosticBS(arr, target, peak+1, arr.length - 1);
         }
         return ans;
     }
