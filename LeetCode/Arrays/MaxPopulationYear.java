b94db5476fe96787980c98bee0754a5dc5b02fb3
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-17 23:13:08 +0530

    maxPopulationYear optimised solution

--- LeetCode/Arrays/MaxPopulationYear.java
+++ LeetCode/Arrays/MaxPopulationYear.java
@@ -16,4 +16,24 @@ public class MaxPopulationYear {
         }
         return maxYear;
     }
+    public int maximumPopulationSoln2(int[][] logs) {
+        int[] arr = new int[101];
+        int maxCnt = 0;
+        int maxYear = 0;
+        for(int i = 0; i < logs.length; i++){
+            int birthYr = logs[i][0];
+            int deathYr = logs[i][1];
+            arr[birthYr - 1950]++;
+            arr[deathYr - 1950]--;
+        }
+        for(int i = 1; i < arr.length; i++){
+            arr[i] += arr[i - 1];
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > maxCnt){
+                maxCnt = arr[i];
+                maxYear = 1950 + i;
+            }
+        }
+        return maxYear;
 }
