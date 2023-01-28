e96ba908cf744c1f32cc2c6c7f23a176765978d8
Krithik22 <krithiksaiprasad22@gmail.com>
2023-01-28 20:56:53 +0530

    selection and bubble sort algorithms completed

---
+++ Algorithms/Searching/SelectionSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    static void selection(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if(arr[max] < arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    public static void main(String[] args) {
+        int[] nums = {5,4,3,2,1};
+        selection(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
