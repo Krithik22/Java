cde8a1dfa5ea8487578aa6d267e79886ecda0356
Krithik22 <krithiksaiprasad22@gmail.com>
2023-07-08 13:52:37 +0530

    length of string using recursion

---
+++ Algorithms/Recursion/LenghtOfString.java
@@ -0,0 +1,13 @@
+public class LenghtOfString {
+    static int len(String str, int i) {
+        if (str.charAt(i) != '\0' && i + 1 < str.length()) {
+            return len(str, i + 1);
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int ans = len("abcd", 0);
+        System.out.println(ans);
+    }
+}
