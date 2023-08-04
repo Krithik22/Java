933a74bd6ef4441075c5787187dff52d2d7a3047
Krithik22 <krithiksaiprasad22@gmail.com>
2023-08-04 22:04:31 +0530

    merge sort algorithm within the array without copying the array

--- Algorithms/Sorting/MergeSort.java
+++ Algorithms/Sorting/MergeSort.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = { 5, 3, 6, 4, 8, 1, 9, 2, 0 };
-        int[] ans = mergeSort(arr);
-        System.out.println(Arrays.toString(ans));
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 
     static int[] mergeSort(int[] arr) {
@@ -40,5 +40,44 @@ public class MergeSort {
         }
 
         return merged;
+    }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int m = (s + e) / 2;
+        mergeSortInPlace(arr, s, m);
+        mergeSortInPlace(arr, m, e);
+
+        mergeInPlace(arr, s, m, e);
+
+    }
+
+    static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] merged = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                merged[k++] = arr[i++];
+            } else {
+                merged[k++] = arr[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = arr[i++];
+        }
+        while (j < e) {
+            merged[k++] = arr[j++];
+        }
+
+        for (int k2 = 0; k2 < merged.length; k2++) {
+            arr[s + k2] = merged[k2];
+        }
     }
 }
