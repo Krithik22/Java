dd851be0a5f929f184487bf21b8e73f5999e01d5
Krithik22 <krithiksaiprasad22@gmail.com>
2023-08-05 22:51:18 +0530

    quick sort done using recursion

---
+++ Algorithms/Sorting/QuickSort.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = { 4, 7, 3, 6, 1, 2 };
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int s = low;
+        int e = high;
+        int m = low + (high - low) / 2;
+        int pivot = arr[m];
+
+        while (s <= e) {
+            while (arr[s] < pivot) {
+                s++;
+            }
+            while (arr[e] > pivot) {
+                e--;
+            }
+
+            if (s <= e) {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        sort(arr, low, e);
+        sort(arr, s, high);
+    }
+}
