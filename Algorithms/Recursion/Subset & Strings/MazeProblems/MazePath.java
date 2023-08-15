9dd8cce9f8ca95313bbacdfb197121fbf37114c0
Krithik22 <krithiksaiprasad22@gmail.com>
2023-08-15 17:59:05 +0530

    maze runner with diagonal

--- Algorithms/Recursion/Subset & Strings/MazeProblems/MazePath.java
+++ Algorithms/Recursion/Subset & Strings/MazeProblems/MazePath.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MazePath {
     public static void main(String[] args) {
         boolean[][] maze = {
@@ -5,7 +7,8 @@ public class MazePath {
                 { true, true, true },
                 { true, true, true }
         };
-        path("", maze, 0, 0);
+        pathWithDiagonal("", maze, 0, 0);
+        // System.out.println(pathList("", maze, 0, 0));
 
     }
 
@@ -19,6 +22,38 @@ public class MazePath {
         }
         if (c < maze[0].length - 1) {
             path(path + 'R', maze, r, c + 1);
+        }
+    }
+
+    static List<String> pathList(String path, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            List<String> list = new ArrayList<>();
+            list.add(path);
+            return list;
+        }
+        List<String> paths = new ArrayList<>();
+        if (r < maze.length - 1) {
+            paths.addAll(pathList(path + 'D', maze, r + 1, c));
+        }
+        if (c < maze[0].length - 1) {
+            paths.addAll(pathList(path + 'R', maze, r, c + 1));
+        }
+        return paths;
+    }
+
+    static void pathWithDiagonal(String path, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(path);
+            return;
+        }
+        if (r < maze.length - 1) {
+            pathWithDiagonal(path + "DOWN - ", maze, r + 1, c);
+        }
+        if (r < maze.length - 1 && c < maze[0].length - 1) {
+            pathWithDiagonal(path + "DIAGONAL - ", maze, r + 1, c + 1);
+        }
+        if (c < maze[0].length - 1) {
+            pathWithDiagonal(path + "RIGHT - ", maze, r, c + 1);
         }
     }
 }
