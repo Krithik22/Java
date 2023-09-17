b31bd510b09b6b2f5c8282f7149b348140ba70d5
Krithik22 <krithiksaiprasad22@gmail.com>
2023-09-17 19:40:51 +0530

    started working in intellij

--- OOPS/Inheritance/Main.java
+++ OOPS/Inheritance/Main.java
@@ -3,6 +3,6 @@ package Inheritance;
 public class Main {
     public static void main(String[] args) {
         Dog a = new Dog(4);
-        a.eat();
+        a.bark();
     }
 }
--- OOPS/Main1.java
+++ OOPS/Main1.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         // Car bmw = new Car();
         // Car ferrari = new Car();
